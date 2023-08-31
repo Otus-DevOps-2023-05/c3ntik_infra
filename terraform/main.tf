b8ad5853a7ac468223017fc68b0a29b1f7738427
c3nt <cent2k2@gmail.com>
2023-08-31 14:08:25 +0700

    terraform-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "0.13.1"
+  required_version = ">= 0.13"
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.93.0"
+      version = "> 0.35"
     }
   }
 }
